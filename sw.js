a0781957288dd3dca5daba0456d3aae98e468792
tiapetronaok-ui <tiapetronaok@gmail.com>
2026-06-04 00:06:54 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,46 @@
-const CACHE_NAME = "tia-petrona-v1";
+const CACHE_NAME = "tia-petrona-v3";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icons/logo.png"
+"/",
+"/index.html",
+"/splash.html",
+"/manifest.json",
+"/icons/logo.png",
+"/icons/icon-192.png",
+"/icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+);
+
+self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys.map(key => {
+if (key !== CACHE_NAME) {
+return caches.delete(key);
+}
+})
+)
+)
+);
+
+self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+
+event.respondWith(
+caches.match(event.request)
+.then(response => response || fetch(event.request))
+);
+
 });
